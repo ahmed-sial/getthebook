31f319b82ecb33c520c1c923b7ded5ef856e6185
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-03-02 05:55:44 +0500

    Initial project setup

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="ahmedhassan"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/java/com/ahmedhassan/getthebook/GetthebookApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedhassan.getthebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetthebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetthebookApplication.class, args);
+	}
+
+}
